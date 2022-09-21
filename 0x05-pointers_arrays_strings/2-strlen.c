9e0e421ab5104436a83436bec1640078d71538d3
Jecolia <jecoliatieleo@gmail.com>
2022-09-20 19:41:07 -0700

    al

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string 
+ *_strlen - returns the length of a string 
  * @s: string to evaluate
  *
  * Return: the length of the string
@@ -12,10 +12,10 @@ int _strlen(char *s)
 
 	i = 0;
 
-	while (s[i] !='\0')
+	while (s[i] != '\0')
 	{
 	      i++;
-        }
+        }  
 
         return (i);
 }	
