641895b2b4d76508016ce29c22c603c0ec96cafa
Jecolia <jecoliatieleo@gmail.com>
2023-06-28 16:12:43 -0700

    prints the number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: number of arguments
+ * @argv: array of size argc
+ *
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc-1);
+
+	return (0);
+	
+}
