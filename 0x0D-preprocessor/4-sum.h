fb7df340a796299c9d8592dcc13b036c82bece6e
Jecolia <jecoliatieleo@gmail.com>
2023-07-20 16:58:22 -0700

    macro that add two number

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * The file: 4-sum.h
+ * Description:computes the sum of two numbers
+ * @x: one of the number
+ * @y: the other number
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
