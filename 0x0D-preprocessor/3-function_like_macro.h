c50d1d9ad9f6ed62ac6f8e22f1a8abab9049e42b
Jecolia <jecoliatieleo@gmail.com>
2023-07-20 07:49:02 -0700

    absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * The file:3-function_like_macro.h
+ * Description:computes the absolute value of a number x
+ * @x: the number to work with
+ */
+
+#define ABS(x) ((x) < (0) ? (-x) : (x))
+
+#endif
