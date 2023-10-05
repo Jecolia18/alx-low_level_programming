c168dbc69ad7ed9fb2b0bd6e080f9ab26576e9c6
Jecolia Tieleo <jecoliatieleo@gmail.com>
2023-10-05 08:28:43 -0700

    print all element in a singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,15 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
-
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
-
 		length++;
 	}
 
