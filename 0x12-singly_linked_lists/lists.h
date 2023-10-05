c979fc7b80829fbb559bbf1da8765255a5deb342
Jecolia Tieleo <jecoliatieleo@gmail.com>
2023-10-05 08:51:00 -0700

    fixing errors

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
  * struct list_s - singly linked list
  * @str: string (malloc'ed string)
  * @len: length of the string
- * @next: points to the next node 
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
@@ -14,7 +14,7 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
