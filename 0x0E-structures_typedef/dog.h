016d87d5c1767808de2d549763be3bde5a950b37
Jecolia Tieleo <jecoliatieleo@gmail.com>
2023-12-11 06:25:48 -0800

    first struct about a dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog{
+/**
+ * struct dog - a structure to a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the name of the owner
+ *
+ * Description: this is related to a dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
